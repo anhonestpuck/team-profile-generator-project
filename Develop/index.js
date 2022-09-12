ebb72728e1882bee68114885ebde01cf8bd2518f
Jeff <pjeffrey40@gmail.com>
2022-09-12 16:34:05 -0400

    second commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,34 @@
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
-
+const Employee = require('./lib/Employee');
+const inquirer = require ('inquirer');
+const fs = require ('fs');
 // TODO: CODE GOES HERE
+
+const teamMembers = [];
+
+function managerInfo () {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'mgrName',
+            message: 'What is the team manager name?'
+        },
+        {
+            type: 'input',
+            name: 'mgrId',
+            message: 'Please enter your ID number'
+        },
+        {
+            type: 'input',
+            name: 'mgrEmail',
+            message: 'Enter your email address'
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'please enter your office number'
+        }
+    ])
+}
