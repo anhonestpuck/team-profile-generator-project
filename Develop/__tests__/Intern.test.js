bd3e70bdd77a5e38c305bafb03b496d88f8bcc1d
Jeff <pjeffrey40@gmail.com>
2022-09-13 14:28:37 -0400

    fixed error with mgr test

--- Develop/__tests__/Intern.test.js
+++ Develop/__tests__/Intern.test.js
@@ -1,7 +1,7 @@
 const Intern = require("../lib/Intern");
 
 describe("Intern is a page to grab functions to make sure that the values are determined for entering them into the html", () => {
-    const schoolValue = 'github.com';
-    const newEng = new Engineer ('name', 'id', 'email', schoolValue)
-    expect(newEng.retrieveGit()).toBe(schoolValue)
+    const universityAttended = 'github.com';
+    const newEng = new Engineer ('name', 'id', 'email', universityAttended)
+    expect(newEng.retrieveGit()).toBe(universityAttended)
 });
--- Develop/__tests__/Manager.test.js
+++ Develop/__tests__/Manager.test.js
@@ -1,6 +1,9 @@
 const Manager = require("../lib/Manager");
 
 describe("Manager", () => {
-
-});
-
+    it('should ensure a value is taken in for placement in the html', () => {
+        const mgrOffice = 3;
+        const mgr = new Manager ('blue', 'carrot', 'happytimes', mgrOffice);
+        expect(mgr.getOffice()).toBe(mgrOffice)
+    } )
+})
